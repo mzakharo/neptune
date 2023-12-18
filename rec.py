303b9c201f4d8cc6ca7cbbb648885807bfe98fea
Mikhail Zakharov <mzakharo@gmail.com>
2023-12-18 14:05:20 -0500

    new rec

--- rec.py
+++ rec.py
@@ -1,10 +1,11 @@
 import paho.mqtt.client as mqtt
 from analyze import ocr, parse
-import numpy as np
 import cv2
 import json
-import sys
 import argparse
+import shlex
+import tempfile
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dump', action='store_true')
@@ -19,35 +20,39 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("flowy/raw")
+    client.subscribe("neptune/ping")
 
 
 prev = 0
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    image_size = (405, 720)
-    img = np.frombuffer(msg.payload, np.uint8).reshape(image_size[1], image_size[0], 4)
-    ocr_err, result, _ = ocr(img)
-    parse_err, consumption = parse(result)
-    err = ocr_err or parse_err
-    filename = f'data/{result}.png'
-    print('err:', err, 'result:', result, filename)
-    if args.dump or (err and args.dump_err):
-        cv2.imwrite(filename, img)
-    if err:
-        return
-    global prev
-    consumption = consumption / 10
-    if 0 <= (consumption-prev) < 50:
-        data = json.dumps(dict(volume=consumption))
-        print('publish', not args.nopublish, data)
-        if not args.nopublish:
-            client.publish("neptune/status", data)
-    prev = consumption
+    with tempfile.NamedTemporaryFile(suffix='.jpeg') as tmp:
+        img_file = tmp.fname
+        subprocess.check_output(shlex.split(f'libcamera-jpeg -n -t 1 -o {img_file}'))
+        img_orig = cv2.imread(img_file)
+        img_orig = cv2.rotate(img_orig, cv2.ROTATE_180)
+        img = cv2.cvtColor(img_orig, cv2.COLOR_BGR2GRAY)
+        ocr_err, result, _ = ocr(img)
+        parse_err, consumption = parse(result)
+        err = ocr_err or parse_err
+        filename = f'data/{result}.png'
+        print('err:', err, 'result:', result, filename)
+        if args.dump or (err and args.dump_err):
+            cv2.imwrite(filename, img)
+        if err:
+            return
+        global prev
+        consumption = consumption / 10
+        if 0 <= (consumption-prev) < 50:
+            data = json.dumps(dict(volume=consumption))
+            print('publish', not args.nopublish, data)
+            if not args.nopublish:
+                client.publish("neptune/status", data)
+        prev = consumption
 
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("nas.local")
+client.connect("nas.home.arpa")
 client.loop_forever()
